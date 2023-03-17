ee53823f32d0957f91ca365424f49039b643fa4b
Léonard Verschuere <leonard.verschuere@epitech.eu>
2023-03-17 22:56:44 +0100

    [ADD] listing function, to list the to-do list

---
+++ src/listing.c
@@ -0,0 +1,34 @@
+/*
+** Léonard Verschuere, 2023
+** side_project
+** listing.c
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include "to_do.h"
+
+int print_list(char *filename)
+{
+    int fd = open(filename, O_RDONLY);
+    struct stat st;
+    char *buffer = NULL;
+
+    stat(filename, &st);
+    if (fd == -1) {
+        printf("\"%s\" could not be opened, please make sure it's a real file.\n", filename);
+        return 84;
+    }
+    buffer = malloc(sizeof(char) * st.st_size + 1);
+    if (read(fd, buffer, st.st_size) == 0) {
+        printf("No task to do\n");
+        return 84;
+    }
+    printf("Here is your To-Do-List:\n%s", buffer);
+    free(buffer);
+    close(fd);
+    return 0;
+}
